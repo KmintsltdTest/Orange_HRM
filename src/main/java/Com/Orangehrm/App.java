1c926282b06ae6e19dee946e0198762c984dd881
Paul Cole <Kmintsltd@gmail.com>
2021-01-01 03:30:26 -0800

    adding initial test framework

---
+++ src/main/java/Com/Orangehrm/App.java
@@ -0,0 +1,13 @@
+package Com.Orangehrm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
